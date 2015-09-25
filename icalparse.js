f37f760c69e26b898dcd1eb299141668c3a40adb
Marc Christophe <m@xtof.net>
2015-09-24 19:57:34 -0700

    think this icalparse.js is working...albeit ugly

--- icalparse.js
+++ icalparse.js
@@ -8,15 +8,154 @@ var moment = require('moment');
 var secret = require('./secret.js');
 
 function orgTimeStamp(start, end) {
-    var res = '';
-    if (end) {
-      res += '<' + moment(start).format('YYYY-MM-DD ddd HH:mm');
-      res += '-' + moment(end).format('HH:mm') + '>';
-    } else {
-      res += '<' + moment(start).format('YYYY-MM-DD ddd') + '>';
-    }
-    return res;
+  var res = '';
+  if (end) {
+    res += '<' + moment(start).format('YYYY-MM-DD ddd HH:mm');
+    res += '-' + moment(end).format('HH:mm') + '>';
+  } else {
+    res += '<' + moment(start).format('YYYY-MM-DD ddd') + '>';
   }
+  return res;
+}
+
+function expandDay(day) {
+  var result;
+  switch (day) {
+    case 'MO':
+      result = 'monday';
+      break;
+    case 'TU':
+      result = 'tuesday';
+      break;
+    case 'WE':
+      result = 'wednesday';
+      break;
+    case 'TH':
+      result = 'thursday';
+      break;
+    case 'FR':
+      result = 'friday';
+      break;
+    case 'SA':
+      result = 'saturday';
+      break;
+    case 'SU':
+      result = 'sunday';
+      break;
+  }
+  return result;
+}
+
+function enumerateDay(day) {
+  var result;
+  switch (day) {
+    case 'MO':
+      result = 1;
+      break;
+    case 'TU':
+      result = 2;
+      break;
+    case 'WE':
+      result = 3;
+      break;
+    case 'TH':
+      result = 4;
+      break;
+    case 'FR':
+      result = 5;
+      break;
+    case 'SA':
+      result = 6;
+      break;
+    case 'SU':
+      result = 0;
+      break;
+  }
+  return result;
+}
+
+function parseRecrrentEvent(res, orgTitle, rrule, dtstart, dtend) {
+  
+  rrule = rrule[0].value;
+
+  var freq = rrule['FREQ'].toLowerCase().slice(0, -2) + 's';
+  var byday = rrule['BYDAY'].split(',');
+  //  console.log(freq, byday);
+  var smoment = moment(dtstart);
+  var emoment = moment(dtend);
+
+  var workingDateS;
+  var workingDateE;
+  
+  switch (freq) {
+    case 'months':
+      workingDateS = moment(smoment).date(1);
+      workingDateE = moment(emoment).date(1);
+      break;
+    case 'weeks':
+      workingDateS = moment(smoment).day('sunday');
+      workingDateE = moment(emoment).day('sunday');
+      break;
+  }
+
+  // console.log('start: ', smoment.format("dddd, MMMM Do YYYY, h:mm:ss a"));
+  // console.log('working: ', workingDate.format("dddd, MMMM Do YYYY, h:mm:ss a"));
+
+  var aMonthFromNow = moment().add(2, 'months');
+  while (workingDateS.isBefore(aMonthFromNow)) {
+
+    // console.log('b -- > ' + workingDate.format('dddd, MMMM Do YYYY, h:mm:ss a'));
+
+    switch (freq) {
+      case 'months':
+
+        var tmS = moment(workingDateS);
+        var tmE = moment(workingDateE);
+
+        for (var i = 0; i < byday.length; i++) {
+
+          var dayn = enumerateDay(byday[i].substr(-2));
+          while (tmS.day() !== dayn) {
+            tmS.add(1, 'day');
+            tmE.add(1, 'day');
+          }
+
+          tmS.add(byday[i][0] - 1, 'weeks');
+          tmE.add(byday[i][0] - 1, 'weeks');
+
+          // console.log(byday[i][0]);
+          // console.log(byday[i].substr(-2));
+          // console.log('@@@ ' + tmS.format("dddd, MMMM Do YYYY, h:mm:ss a") + ' -- ' + tmE.format("dddd, MMMM Do YYYY, h:mm:ss a"));
+          
+          res += orgTitle;
+          res += '  ' + orgTimeStamp(tmS, tmE) + '\n';
+        }
+
+        break;
+      case 'weeks':
+
+        var tmS = moment(workingDateS);
+        var tmE = moment(workingDateE);
+        
+        for (var i = 0; i < byday.length; i++) {
+          var day = expandDay(byday[i]);
+          tmS.day(day);
+          tmE.day(day);
+            //console.log('### ' + tmS.format("dddd, MMMM Do YYYY, h:mm:ss a") + ' -- ' + tmE.format("dddd, MMMM Do YYYY, h:mm:ss a"));
+          res += orgTitle;
+          res += '  ' + orgTimeStamp(tmS, tmE) + '\n';
+        }
+        break;
+    }
+
+    workingDateS.add(1, freq);
+    workingDateE.add(1, freq);
+    // console.log('e -- > ' + workingDate.format('dddd, MMMM Do YYYY, h:mm:ss a'));
+  }
+
+  return res;
+  
+}
 
 request(secret[0].src, function(error, response, body) {
   if (error) throw (error);
@@ -25,6 +164,7 @@ request(secret[0].src, function(error, response, body) {
   var ical = icalendar.parse_calendar(body);
 
   ical.events().forEach(function(item) {
+    
     var props = item.properties;
     var summary = props['SUMMARY'];
     var orgTitle = '* ' + summary[0].value + '\n';
@@ -32,97 +172,25 @@ request(secret[0].src, function(error, response, body) {
     var dtstart = props['DTSTART'][0].value;
     var dtend = props['DTEND'][0].value;
     var recurrence_id = props['RECURRENCE-ID'];
+
     if (rrule) {
-      rrule = rrule[0].value;
 
-      var freq = rrule['FREQ'].toLowerCase().slice(0, -2) + 's';
-      var byday = rrule['BYDAY'].split(',');
+      res += parseRecrrentEvent(res, orgTitle, rrule, dtstart, dtend);
 
-      res += orgTitle;
-
-      console.log(freq, byday);
-
-      var smoment = moment(dtstart);
-      var emoment = moment(dtend);
-
-      var workingDate;
-      switch (freq) {
-        case 'months':
-          workingDate = moment(smoment).date(1);
-          break;
-        case 'weeks':
-          workingDate = moment(smoment).day('sunday');
-          break;
-      }
-
-      console.log(smoment.format("dddd, MMMM Do YYYY, h:mm:ss a"));
-      console.log(workingDate.format("dddd, MMMM Do YYYY, h:mm:ss a"));
-      // console.log(rrule);
-
-      var aMonthFromNow = moment().add(1, 'months');
-      // // console.log(aMonthFromNow.format("dddd, MMMM Do YYYY, h:mm:ss a"));
-      while (workingDate.isBefore(aMonthFromNow)) {
-
-        switch (freq) {
-          case 'months':
-                
-            break;
-          case 'weeks':
-            var tm = moment(workingDate);
-            for(var i=0; i<byday.length; i++){
-              var day = byday[i];
-              switch(day){
-                case 'MO':
-                  tm.day('monday');
-                  break;
-                case 'TU':
-                  tm.day('tuesday');
-                  break;
-                case 'WE':
-                  tm.day('wednesday');
-                  break;
-                case 'TH':
-                  tm.day('thursday');
-                  break;
-                case 'FR':
-                  tm.day('friday');
-                  break;
-                case 'SA':
-                  tm.day('saturday');
-                  break;
-                case 'SU':
-                  tm.day('sunday');
-                  break;
-              }
-              console.log('### ' + tm.format("dddd, MMMM Do YYYY, h:mm:ss a"));
-            }
-            // switch(){
-
-            // }
-            break;
-        }
-        
-        workingDate.add(1, freq);
-        console.log(' -- > ' + workingDate.format('dddd, MMMM Do YYYY, h:mm:ss a'));
-      }
-
-      // if (dtstart.date_only) {
-      //   res += '  ' + orgTimeStamp(dtstart) + '\n';
-      // } else {
-      //   res += '  ' + orgTimeStamp(dtstart, dtend) + '\n';
-      // }
     } else {
+
+      console.log(dtstart);
       if (recurrence_id === undefined) {
         res += orgTitle;
         if (dtstart.date_only) {
           res += '  ALL DAY\n';
         } else {
           res += '  ' + orgTimeStamp(dtstart, dtend) + '\n';
-          // res += '  NOT ALL DAY\n';
         }
       } else {
         //res += orgTitle + '  this is recurring\n';
       }
+      
     }
   });
   saveOrgFile(res);
@@ -138,117 +206,3 @@ var saveOrgFile = function(dataStr) {
     // }
   });
 };
-
-// {
-//   DTSTART: [{
-//     type: 'DATE-TIME',
-//     name: 'DTSTART',
-//     value: Fri Jul 03 2015 12: 30: 00 GMT - 0700(PDT),
-//     parameters: [Object]
-//   }],
-//   DTEND: [{
-//     type: 'DATE-TIME',
-//     name: 'DTEND',
-//     value: Fri Jul 03 2015 13: 30: 00 GMT - 0700(PDT),
-//     parameters: [Object]
-//   }],
-//   RRULE: [{
-//     type: 'RECUR',
-//     name: 'RRULE',
-//     value: [Object],
-//     parameters: {}
-//   }],
-//   EXDATE: [{
-//     type: 'DATE-TIME',
-//     name: 'EXDATE',
-//     value: [Object],
-//     parameters: [Object]
-//   }],
-//   DTSTAMP: [{
-//     type: 'DATE-TIME',
-//     name: 'DTSTAMP',
-//     value: Wed Sep 23 2015 13: 39: 31 GMT - 0700(PDT),
-//     parameters: {}
-//   }],
-//   ORGANIZER: [{
-//     type: 'CAL-ADDRESS',
-//     name: 'ORGANIZER',
-//     value: 'mailto:bianca@telegraphacademy.com',
-//     parameters: [Object]
-//   }],
-//   UID: [{
-//     type: 'TEXT',
-//     name: 'UID',
-//     value: '6k0rkac8egtrb0u61n1c8hp5hc@google.com',
-//     parameters: {}
-//   }],
-//   ATTENDEE: [{
-//     type: 'CAL-ADDRESS',
-//     name: 'ATTENDEE',
-//     value: 'mailto:shanti@telegraphacademy.com',
-//     parameters: [Object]
-//   }, {
-//     type: 'CAL-ADDRESS',
-//     name: 'ATTENDEE',
-//     value: 'mailto:openarms@telegraphacademy.com',
-//     parameters: [Object]
-//   }, {
-//     type: 'CAL-ADDRESS',
-//     name: 'ATTENDEE',
-//     value: 'mailto:bianca@telegraphacademy.com',
-//     parameters: [Object]
-//   }, {
-//     type: 'CAL-ADDRESS',
-//     name: 'ATTENDEE',
-//     value: 'mailto:marc.christophe@telegraphacademy.com',
-//     parameters: [Object]
-//   }],
-//   CREATED: [{
-//     type: 'DATE-TIME',
-//     name: 'CREATED',
-//     value: Wed Jul 01 2015 15: 59: 51 GMT - 0700(PDT),
-//     parameters: {}
-//   }],
-//   DESCRIPTION: [{
-//     type: 'TEXT',
-//     name: 'DESCRIPTION',
-//     value: '',
-//     parameters: {}
-//   }],
-//   'LAST-MODIFIED': [{
-//     type: 'DATE-TIME',
-//     name: 'LAST-MODIFIED',
-//     value: Thu Sep 17 2015 21: 43: 56 GMT - 0700(PDT),
-//     parameters: {}
-//   }],
-//   LOCATION: [{
-//     type: 'TEXT',
-//     name: 'LOCATION',
-//     value: '',
-//     parameters: {}
-//   }],
-//   SEQUENCE: [{
-//     type: 'INTEGER',
-//     name: 'SEQUENCE',
-//     value: '0',
-//     parameters: {}
-//   }],
-//   STATUS: [{
-//     type: 'TEXT',
-//     name: 'STATUS',
-//     value: 'CONFIRMED',
-//     parameters: {}
-//   }],
-//   SUMMARY: [{
-//     type: 'TEXT',
-//     name: 'SUMMARY',
-//     value: 'All Hands Meeting',
-//     parameters: {}
-//   }],
-//   TRANSP: [{
-//     type: 'TEXT',
-//     name: 'TRANSP',
-//     value: 'OPAQUE',
-//     parameters: {}
-//   }]
-// };
